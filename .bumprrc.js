9e164aa7c4bf0e679a0a6ab84922311d77e0ea3c
Bumpr <bumpr@domain.com>
2019-04-17 12:31:13 -0600

    Add files needed to try bumpr releases

---
+++ .bumprrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  features: {
+    changelog: {
+      enabled: true
+    },
+    comments: {
+      enabled: true
+    },
+    logging: {
+      enabled: true,
+      file: '.bumpr-log.json'
+    },
+    release: {
+      enabled: true
+    }
+  }
+}
